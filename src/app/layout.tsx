c9b925503b53f365c5dd9da833c73d2e166df1ac
shigeruogawa <ogawa.shigeru1@gmail.com>
2024-06-30 16:47:29 +0900

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const c = 'a'
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
